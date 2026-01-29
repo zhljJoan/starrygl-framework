23c50103b882fcc454f881953d9e71e2c14ad3cc
zhlj <12221136@zju.edu.cn>
2026-01-29 11:41:06 +0800

    fix prepare pipeline

--- starrygl/utils/params.py
+++ starrygl/utils/params.py
@@ -26,7 +26,7 @@ TEST_CONFIG = {
         "chunks_per_batch":4,
         "shuffle_chunks":True,
         'sample_type':'uniform', 
-        'layers':2, 
+        'layers':1, 
         'fanout':10, 
         'allowed_offset':-1,
         'equal_root_time':False, 
@@ -37,10 +37,10 @@ TEST_CONFIG = {
     "train": {
         "self_loop": True,
         "memory_type":"GRU",
-        "dim_time": 16,
-        "hidden_dim": 16,
+        "dim_time": 100,
+        "hidden_dim": 100,
         "num_heads": 2,
-        "num_layers": 2,
+        "num_layers": 1,
         "dropout": 0.1,
         "att_dropout": 0.1,
         "lr":0.0001
